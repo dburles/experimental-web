1f9a22027c4f661b6c8b493f2d674a6b6179c835
David Burles <dburles@gmail.com>
2024-12-28 13:30:34 +1100

    encoding

--- scripts/lib/generateIndexHtml.js
+++ scripts/lib/generateIndexHtml.js
@@ -3,7 +3,7 @@
 import { readFile } from "node:fs/promises";
 
 export default async function generateIndexHtml() {
-  const html = (await readFile("./index.html")).toString();
+  const html = await readFile("./index.html", { encoding: "utf-8" });
   const importMap = await readFile("./importmap.json", { encoding: "utf-8" });
   return html.replace(
     "<!-- {importmap} -->",
